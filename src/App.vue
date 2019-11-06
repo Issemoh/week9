cf9263033327f131a072461ee44090ab1ff8f2f8
Ice <your name>
2019-11-06 14:42:42 -0600

    stil working on

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+    <StudentTable v-bind:students="students" v-on:student-present="studentArrivedOrleft"></StudentTable>
+    <StudentMessage></StudentMessage>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
+
 
 export default {
   name: 'app',
+  data() {
+    return {
+       students: [],
+    }
+  },
   components: {
-    HelloWorld
+   NewStudentForm,
+   StudentTable,
+   StudentMessage
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student)
+      this.students.sort(function(s1, s2) {
+        return s1.name.toLowerCase() > s2.name.toLowerCase() ? -1: 1
+      })
+    },
+    studentArrivedOrLeft(student) {
+      this.message =  student.present ? 'Welcome,' : 'Goodbye,'
+      this.name = student.name
+    }
   }
 }
 </script>
